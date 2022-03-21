faa089493f1867af552faa062c0d0f3a9cf99763
Александр Ушаков <ushakoff1708BT@yandex.ru>
2022-03-21 22:25:50 +0300

    task with security context

--- src/main/java/security/controller/HelloController.java
+++ src/main/java/security/controller/HelloController.java
@@ -1,13 +1,32 @@
 package security.controller;
 
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "hello!";
+    public String hello(Authentication authentication) throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        try {
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Hi, " + executorService.submit(contextTask).get() + "!";
+        } finally {
+            executorService.shutdown();
+        }
     }
 }
