4fbfdc660248ca3ac0b2cefe296d98bc371be327
Александр Ушаков <ushakoff1708BT@yandex.ru>
2022-03-21 15:44:23 +0300

    another config

--- src/main/java/security/ProjectConfig.java
+++ src/main/java/security/ProjectConfig.java
@@ -4,31 +4,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        UserDetails user = User.withUsername("john")
+        auth.inMemoryAuthentication()
+            .withUser("john")
             .password("12345")
             .authorities("read")
-            .build();
-
-        userDetailsService.createUser(user);
-
-        auth.userDetailsService(userDetailsService)
+            .and()
             .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
