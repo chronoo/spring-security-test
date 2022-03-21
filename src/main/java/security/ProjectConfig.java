e04b77bc1cabd8116c97f629d48a3289d33adf2e
Александр Ушаков <ushakoff1708BT@yandex.ru>
2022-03-21 12:49:42 +0300

    permit all request

--- src/main/java/security/ProjectConfig.java
+++ src/main/java/security/ProjectConfig.java
@@ -2,6 +2,8 @@ package security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,7 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -24,6 +26,12 @@ public class ProjectConfig {
         userDetailsService.createUser(user);
 
         return userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().permitAll();
     }
 
     @Bean
