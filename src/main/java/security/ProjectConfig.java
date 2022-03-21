79c545b15f160a2b5ecde36892b335535e7952fa
Александр Ушаков <ushakoff1708BT@yandex.ru>
2022-03-21 12:56:29 +0300

    another config

--- src/main/java/security/ProjectConfig.java
+++ src/main/java/security/ProjectConfig.java
@@ -1,21 +1,18 @@
 package security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    public UserDetailsService userDetailsService() {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         var userDetailsService = new InMemoryUserDetailsManager();
 
         UserDetails user = User.withUsername("john")
@@ -25,17 +22,13 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
         userDetailsService.createUser(user);
 
-        return userDetailsService;
+        auth.userDetailsService(userDetailsService)
+            .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();
         http.authorizeRequests().anyRequest().permitAll();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 }
