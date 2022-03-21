ef401e1636d2ce82681c43923f284e4106b77a21
Александр Ушаков <ushakoff1708BT@yandex.ru>
2022-03-21 15:49:27 +0300

    custom auth-provider

--- src/main/java/security/ProjectConfig.java
+++ src/main/java/security/ProjectConfig.java
@@ -1,21 +1,19 @@
 package security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomAuthenticationProvider authenticationProvider;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-            .withUser("john")
-            .password("12345")
-            .authorities("read")
-            .and()
-            .passwordEncoder(NoOpPasswordEncoder.getInstance());
+        auth.authenticationProvider(authenticationProvider);
     }
 
     @Override
