65b8d98a2152b22cb0f28fceb0d41a4935c27c02
Александр Ушаков <ushakoff1708BT@yandex.ru>
2022-03-22 00:53:18 +0300

    add liquibase

--- src/main/java/security/SecurityApplication.java
+++ src/main/java/security/SecurityApplication.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
+import security.entity.SimpleUser;
 import security.repository.UserRepository;
 
 import javax.annotation.PostConstruct;
@@ -22,6 +23,10 @@ class StartupService {
 
     @PostConstruct
     public void init() {
+        if (repository.count() == 0) {
+            SimpleUser defaultUser = new SimpleUser("alex", "123", "read");
+            repository.save(defaultUser);
+        }
         repository.findAll().forEach(System.out::println);
     }
 }
