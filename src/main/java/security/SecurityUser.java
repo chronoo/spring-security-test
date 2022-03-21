5ab39a951c35fce560115b53bc8330d9372e83ed
Александр Ушаков <ushakoff1708BT@yandex.ru>
2022-03-21 18:01:34 +0300

    add user dto

--- src/main/java/security/SecurityUser.java
+++ src/main/java/security/SecurityUser.java
@@ -6,28 +6,26 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class SimpleUser implements UserDetails {
-    private final String username;
-    private final String password;
+public class SecurityUser implements UserDetails {
+    private final User user;
 
-    public SimpleUser(String username, String password) {
-        this.username = username;
-        this.password = password;
+    public SecurityUser(User user) {
+        this.user = user;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> "READ");
+        return List.of(() -> user.getAuthority());
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return username;
+        return user.getUsername();
     }
 
     @Override
