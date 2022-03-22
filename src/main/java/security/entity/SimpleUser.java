0d35aa3655345a6c766d670da2702dab2cc23c9b
Александр Ушаков <ushakoff1708BT@yandex.ru>
2022-03-22 09:57:55 +0300

    use lombok for entity

--- src/main/java/security/entity/SimpleUser.java
+++ src/main/java/security/entity/SimpleUser.java
@@ -1,5 +1,8 @@
 package security.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -7,6 +10,9 @@ import javax.persistence.*;
 import java.util.Collection;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 public class SimpleUser implements UserDetails {
     @Id
@@ -19,14 +25,6 @@ public class SimpleUser implements UserDetails {
     private String password;
 
     private String authority;
-
-    public SimpleUser() {}
-
-    public SimpleUser(String username, String password, String authority) {
-        this.username = username;
-        this.password = password;
-        this.authority = authority;
-    }
 
     @Override
     public boolean isAccountNonExpired() {
@@ -53,40 +51,9 @@ public class SimpleUser implements UserDetails {
         return List.of(() -> authority);
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAuthority() {
-        return authority;
-    }
-
-    public void setAuthority(String authority) {
-        this.authority = authority;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
     @Override
     public String toString() {
         return username + ":" + password;
     }
 }
+
